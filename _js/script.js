cea10d3d3a959da0d1b52ae172ccedbe9af8f402
Emmy <emmy.917.ez@gmail.com>
2016-02-20 07:19:34 -0500

    new commit

---
+++ _js/script.js
@@ -0,0 +1,9 @@
+/* $(function () {
+     $('.tlt').textillate({
+       in: { effect: 'fadeIn', sync: true }
+   });
+ })*/
+
+ $(document).ready(function(){
+     $(".all").hide().fadeIn(1000);
+ });
